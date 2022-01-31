d57d10c6598a68360e164325058a27090bec301c
nohsangwoo <nsgr12@naver.com>
2022-02-01 08:27:45 +0900

    fetching data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,29 @@
-import Head from 'next/head'
+import { useEffect, useState } from 'react'
 import Seo from '../components/Seo'
 
-const Home = () => {
-  const asdfasdf = 'asdfasdfasd'
+const API_KEY = '200aa47b7c88f369e8d2cd6d8db21dfb'
+
+export default function Home() {
+  const [movies, setMovies] = useState([])
+  useEffect(() => {
+    ;(async () => {
+      const { results } = await (
+        await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+        )
+      ).json()
+      setMovies(results)
+    })()
+  }, [])
   return (
     <div>
       <Seo title="Home" />
-      <h1>hello</h1>
+      {!movies && <h4>Loading...</h4>}
+      {movies?.map((movie: any) => (
+        <div key={movie.id}>
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
     </div>
   )
 }
-
-export default Home
