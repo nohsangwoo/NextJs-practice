077eab0313a60005df6639b42c5899cbcde00f7a
nohsangwoo <nsgr12@naver.com>
2022-02-03 11:41:06 +0900

    server side rendering - getServerSideProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,22 @@
 import { useEffect, useState } from 'react'
 import Seo from '../components/Seo'
 
-export default function Home() {
-  const [movies, setMovies] = useState([])
-  useEffect(() => {
-    ;(async () => {
-      const { results } = await (await fetch(`/api/movies`)).json()
-      setMovies(results)
-    })()
-  }, [])
+interface Props {
+  results: any
+}
+export default function Home({ results }: Props) {
+  // const [movies, setMovies] = useState([])
+  // useEffect(() => {
+  //   ;(async () => {
+  //     const { results } = await (await fetch(`/api/movies`)).json()
+  //     setMovies(results)
+  //   })()
+  // }, [])
   return (
     <div className="container">
       <Seo title="Home" />
-      {!movies && <h4>Loading...</h4>}
-      {movies?.map((movie: any) => (
+      {/* {movies.length === 0 && <h4>Loading...</h4>} */}
+      {results?.map((movie: any) => (
         <div className="movie" key={movie.id}>
           <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
           <h4>{movie.original_title}</h4>
@@ -42,4 +45,15 @@ export default function Home() {
       `}</style>
     </div>
   )
+}
+
+// getServerSideProps은 function이름을 변경하면 안된다
+export async function getServerSideProps() {
+  const { results } = await (
+    await fetch(`http://localhost:3000/api/movies`)
+  ).json()
+
+  return {
+    props: { results },
+  }
 }
